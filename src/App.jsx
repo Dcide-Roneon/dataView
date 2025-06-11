4d55aa9e45dea204098e3202ee0f12e91aee4314
Dcide-Roneon <jordan.fenty@dcide.co.uk>
2025-06-11 14:00:08 +0100

    Need to move this repo to local machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Container, Typography, Button } from '@mui/material';
 import Home from './pages/home.jsx';
+import Toolbar from './pages/frontPage.jsx';
 
 function App() {
   return (
-    <Home/>
+    <Toolbar/>
   );
 }
 
